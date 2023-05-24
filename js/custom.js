92356e826a8e3fdfa106ae8ada6664c29c769b90
Sultan <sultan10081995@gmail.com>
2023-05-24 16:47:52 +0600

    first commit

---
+++ js/custom.js
@@ -0,0 +1,34 @@
+$(function(){
+  // ===== background js starts =====
+  $(".spring").click(function(){
+    $(".spring-bg").fadeIn().siblings().fadeOut();
+  });
+
+  $(".summer").click(function(){
+    $(".summer-bg").fadeIn().siblings().fadeOut();
+  });
+
+  $(".monsoon").click(function(){
+    $(".monsoon-bg").fadeIn().siblings().fadeOut();
+  });
+
+  $(".autumn").click(function(){
+    $(".autumn-bg").fadeIn().siblings().fadeOut();
+  });
+
+  $(".p-winter").click(function(){
+    $(".p-winter-bg").fadeIn().siblings().fadeOut();
+  });
+
+  $(".winter").click(function(){
+    $(".winter-bg").fadeIn().siblings().fadeOut();
+  });
+  // ===== background js ends =====
+  // ===== toggler js starts =====
+  $(".toggler").click(function(){
+    $(".list").slideToggle();
+  });
+  // ===== toggler js ends =====
+
+
+});
